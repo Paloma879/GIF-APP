cce49eb05ebe97482d3598afaeabf93c934c58f4
Paloma <palomammartinez17@gmail.com>
2022-10-02 00:01:26 -0300

    Api key

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -2,7 +2,17 @@
 const GifGrid = ({ category }) => {
 //Recordar el useState es se pone entre corchetes
 
+const getGifs = async () =>{
+  const API_KEY ="6opHtBFaDs3uhQ1Z2qgPTTYWRPWBKO8a";
+  const url =`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${10}&offset=0&rating=g&lang=en`;
 
+  const resp = await fetch (url);
+
+  const { data } = await resp.json()
+  console.log(data);
+}
+
+getGifs();
 return (
     <>
       <h3>{category}</h3>
