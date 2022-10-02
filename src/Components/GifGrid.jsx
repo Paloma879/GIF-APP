88af4e0bde0ff9afdf992b58bbcf7d650083d160
Paloma <palomammartinez17@gmail.com>
2022-10-02 00:10:54 -0300

    gifs

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -1,24 +1,29 @@
-
 const GifGrid = ({ category }) => {
-//Recordar el useState es se pone entre corchetes
+  //Recordar el useState es se pone entre corchetes
 
-const getGifs = async () =>{
-  const API_KEY ="6opHtBFaDs3uhQ1Z2qgPTTYWRPWBKO8a";
-  const url =`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${10}&offset=0&rating=g&lang=en`;
+  const getGifs = async () => {
+    const API_KEY = "6opHtBFaDs3uhQ1Z2qgPTTYWRPWBKO8a";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${10}&offset=0&rating=g&lang=en`;
 
-  const resp = await fetch (url);
+    const resp = await fetch(url);
 
-  const { data } = await resp.json()
-  console.log(data);
-}
+    const { data } = await resp.json();
+    const gifs = data.map(img => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.original
+    }))
 
-getGifs();
-return (
+    console.log(gifs);
+  };
+
+  getGifs();
+  return (
     <>
       <h3>{category}</h3>
-    
-        {/* Desplegar listado*/}
+
+      {/* Desplegar listado*/}
     </>
   );
 };
-export default GifGrid
+export default GifGrid;
